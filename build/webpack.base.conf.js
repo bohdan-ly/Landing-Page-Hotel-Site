cd2a24eb7d6feb895c4701b412298f2ee4de139b
dronkfc@gmail.com <dronkfc@gmail.com>
2020-05-03 04:45:19 +0300

    webpack setup + pug + scss + header

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,150 @@
+/* Base config:
+   ========================================================================== */
+
+const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Main const. Feel free to change it
+const PATHS = {
+	src: path.join(__dirname, '../src'),
+	dist: path.join(__dirname, '../dist'),
+	assets: 'assets/'
+};
+
+// Pages const for HtmlWebpackPlugin
+// see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter((fileName) => fileName.endsWith('.pug'));
+
+module.exports = {
+	externals: {
+		paths: PATHS
+	},
+	entry: {
+		app: PATHS.src
+		// module: `${PATHS.src}/your-module.js`,
+	},
+	output: {
+		filename: `${PATHS.assets}js/[name].[contenthash].js`,
+		path: PATHS.dist,
+		publicPath: '/'
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					name: 'vendors',
+					test: /node_modules/,
+					chunks: 'all',
+					enforce: true
+				}
+			}
+		}
+	},
+	module: {
+		rules: [
+			{
+				// JavaScript
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules/'
+			},
+			{
+				// JavaScript
+				test: /\.pug$/,
+				loader: 'pug-loader'
+			},
+			{
+				// Fonts
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]'
+				}
+			},
+			{
+				// images / icons
+				test: /\.(png|jpg|gif|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]'
+				}
+			},
+			{
+				// scss
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true }
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							config: { path: `./postcss.config.js` }
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: { sourceMap: true }
+					}
+				]
+			},
+			{
+				// css
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { sourceMap: true }
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							config: { path: `./postcss.config.js` }
+						}
+					}
+				]
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'~': PATHS.src
+		}
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: `${PATHS.assets}css/[name].[contenthash].css`
+		}),
+		new CopyWebpackPlugin([
+			{ from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
+			{ from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+			{ from: `${PATHS.src}/static`, to: '' }
+		]),
+
+		/*
+      Automatic creation any html pages (Don't forget to RERUN dev server!)
+      See more:
+      https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
+      Best way to create pages:
+      https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
+    */
+		...PAGES.map(
+			(page) =>
+				new HtmlWebpackPlugin({
+					template: `${PAGES_DIR}/${page}`, //input pug
+					filename: `./${page.replace(/\.pug/, '.html')}` //output html
+				})
+		)
+	]
+};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// JS
+import './js/';
+
+// SCSS
+import './assets/scss/main.scss';
+
+// CSS (example)
+// import './assets/css/main.css'
